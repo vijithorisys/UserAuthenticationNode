a3d4a2a48b464d13a1ed4bd3020ea584dafff274
vijith.p <vijith.p@orisys.in>
2024-04-19 18:16:49 +0530

    router update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import EmployeeList from "./components/employee/EmployeeList";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path='/' element={<Login/>}></Route>
           <Route path='/register' element={<Register/>}></Route>
           <Route path='/employee' element={<EmployeeList/>}></Route>
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   );
 };
